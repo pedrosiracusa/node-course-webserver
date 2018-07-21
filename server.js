0eb64b2cc08724f724acb49c293a7d50d81d6641
Pedro Correia de Siracusa <pedrosiracusa@gmail.com>
2018-07-21 00:59:37 -0300

    Adding projects page

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use( (req,res,next)=>{
     next() ;
 });
 
-app.use( (req, res, next)=>{
-    res.render('maintenance.hbs');
-});
+// app.use( (req, res, next)=>{
+//     res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -42,6 +42,12 @@ app.get('/', (req,res)=>{
 app.get('/about', (req,res)=>{
     res.render('about.hbs',{
         pageTitle: 'About Page',
+    });
+});
+
+app.get('/projects', (req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page',
     });
 });
 
